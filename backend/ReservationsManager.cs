cf372384f6a2b73a79f9cd55db8b076e5ba938df
Kerisha Stewart <kk.stewart001@gmail.com>
2023-07-05 22:53:25 -0400

    backend initial

---
+++ backend/ReservationsManager.cs
@@ -0,0 +1,10 @@
+namespace backend 
+{
+    public static class ReservationsManager
+    {
+       public static void MapReservations(this WebApplication app)
+       {
+        
+       }
+    }
+}
